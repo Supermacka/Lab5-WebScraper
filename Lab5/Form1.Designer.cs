6738c7337f4f874f6e696ea2e29dbf4a591c76d9
Colin Farkas <colin.farkas@iths.se>
2020-11-06 16:38:11 +0100

    Changes Pushed

---
+++ Lab5/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.buttonExtract = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.labelCountImages = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(133, 47);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(414, 20);
+            this.textBoxURL.TabIndex = 0;
+            // 
+            // buttonExtract
+            // 
+            this.buttonExtract.BackColor = System.Drawing.Color.Indigo;
+            this.buttonExtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExtract.ForeColor = System.Drawing.Color.Cornsilk;
+            this.buttonExtract.Location = new System.Drawing.Point(571, 45);
+            this.buttonExtract.Name = "buttonExtract";
+            this.buttonExtract.Size = new System.Drawing.Size(75, 23);
+            this.buttonExtract.TabIndex = 1;
+            this.buttonExtract.Text = "Extract";
+            this.buttonExtract.UseVisualStyleBackColor = false;
+            this.buttonExtract.Click += new System.EventHandler(this.buttonExtract_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(133, 85);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(513, 264);
+            this.listBox1.TabIndex = 2;
+            // 
+            // labelCountImages
+            // 
+            this.labelCountImages.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelCountImages.AutoSize = true;
+            this.labelCountImages.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCountImages.ForeColor = System.Drawing.Color.Indigo;
+            this.labelCountImages.Location = new System.Drawing.Point(130, 394);
+            this.labelCountImages.Name = "labelCountImages";
+            this.labelCountImages.Size = new System.Drawing.Size(101, 13);
+            this.labelCountImages.TabIndex = 3;
+            this.labelCountImages.Text = "Found: X images";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.buttonSave.Location = new System.Drawing.Point(524, 388);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(122, 23);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save Images";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.progressBar1.ForeColor = System.Drawing.Color.Orange;
+            this.progressBar1.Location = new System.Drawing.Point(307, 394);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(201, 13);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelCountImages);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonExtract);
+            this.Controls.Add(this.textBoxURL);
+            this.Name = "Form1";
+            this.Text = "t";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button buttonExtract;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label labelCountImages;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Lab5/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExtract_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            if (textBoxURL.Text.Contains(@"https://"))
+            {
+                // Download WebURL
+                Task<string> downloadURL = client.GetStringAsync(textBoxURL.Text);
+                downloadURL.Wait();
+
+                // Regex expression
+                string pattern = "<img.*src=\"(.*?)\"";
+                Regex rgx = new Regex(pattern);
+                   
+                // Variables
+                string imgLink = "";
+                int found = 0;
+                
+                // Clear list if user would download more images
+                listBox1.Items.Clear();
+
+                foreach (Match imgMatch in rgx.Matches(downloadURL.Result.ToString()))
+                {
+                    //Making sure only links with full url are read
+                    if (imgMatch.Groups[1].Length > @"https://gp.se".Length)
+                    {
+                        if (!imgMatch.ToString().Contains(@"https://"))
+                        {
+                            imgLink = imgMatch.Groups[1].ToString().Insert(0, @"https://gp.se");
+                        }
+                        else
+                        {
+                            imgLink = imgMatch.Groups[1].ToString();
+                        }
+                        listBox1.Items.Add(imgLink);
+                        found++;
+                    }
+                }
+                // Change label 
+                labelCountImages.Text = $"Found: {found} images";
+
+                // Change control properties
+                buttonSave.ForeColor = Color.White;
+                buttonSave.BackColor = Color.Indigo;
+                buttonSave.Enabled = true;
+            }
+            //If URL is not valid
+            else
+            {
+                string message = "Enter the full URL-name";
+                string title = "Could not find URL";
+                MessageBox.Show(message, title);
+            }
+        }
+
+        private async void buttonSave_Click(object sender, EventArgs e)
+        {
+            // File types
+            string[] fileEndings = new string[] { "bmp", "png", "gif", "jpg", "jpeg" };
+
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK && !Directory.EnumerateFileSystemEntries(dialog.SelectedPath).Any())
+                {
+                    int imageName = 1;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = listBox1.Items.Count - 1;
+                    buttonSave.Text = "Downloading...";
+
+                    foreach (string item in listBox1.Items)
+                    {
+                        if (item.ToString().Length != 0)
+                        {
+                            // Determine filetype and add to file-name 
+                            for (int j = 0; j < fileEndings.Length; j++)
+                            {
+                                if (item.Contains(fileEndings[j]))
+                                {
+                                    string fileType = $".{fileEndings[j]}";
+
+                                    // Download img from url and add to list. Then save to directory.
+                                    var list = new List<Task>();
+                                    using (WebClient client = new WebClient())
+                                    {
+                                        var task = client.DownloadFileTaskAsync(new Uri(item), $@"{dialog.SelectedPath}\Image{imageName}{fileType}");
+                                        list.Add(task);
+                                        progressBar1.Value++;
+                                    }
+                                    await Task.WhenAny(list);
+                                    break;
+                                }
+                            }
+                            imageName++;
+
+                            // Change label 
+                            labelCountImages.Text = $"Downloading: {imageName} images";
+                        }
+                    }
+                    buttonSave.Text = "Completed";
+                    buttonSave.BackColor = Color.Green;
+                    buttonSave.Enabled = false;
+                    
+                    labelCountImages.Text = $"{imageName} images downloaded";
+                    labelCountImages.ForeColor = Color.Green;
+                }
+                // If directory-folder contains exisiting files
+                else
+                {
+                    string message = "Choose a folder that is empty";
+                    string title = "Folder not empty!";
+                    MessageBox.Show(message, title);
+                }
+            }
+        }
+    }
+}
